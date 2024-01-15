35cfe1cbbad5049e31aa7cb8e0a4fd807b276d2f
sura1717 <surajkushwaha1717@gmail.com>
2024-01-15 13:54:18 -0800

    add a new function

--- mathmatical_fn_in_numpy.py
+++ mathmatical_fn_in_numpy.py
@@ -4,10 +4,12 @@ def sigmoid(arr):
 
 def MSE(arr1,arr2):
     return np.mean((arr1-arr2)**2)
-
+"""def BCE(arr1,arr2):
+    return np.mean(-((arr2*(np.log(1-arr1)))+(1-arr2)*(np.log(1-arr1))))"""
 a=np.random.randint(1,50,20)
 b=np.random.randint(1,50,20)
 print("sigmoid of whole array a is ",sigmoid(a))
+#print(BCE(a,b))          this function are not working formula is wrong
 print()
 print("sigmoid of whole array b is ",sigmoid(b))
 print()
